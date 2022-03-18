6537896f6f94af1ad178c6ef78381cf6691ab088
Rohit Singh <singh.rohitsingh2k@gmail.com>
2022-03-18 23:55:05 +0530

    simple modal implementation using <dialog>

---
+++ index.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-btn');
+const openModal = document.querySelector('.open-btn');
+
+
+
+openModal.addEventListener('click', () => {
+    modal.show();
+})
